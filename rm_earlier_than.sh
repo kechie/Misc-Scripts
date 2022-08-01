8c4b3d42c37b5180b63f2436ff97011a8575fb3d
Richard L. Alhama <richard.alhama@gmail.com>
2022-08-01 15:52:20 +0800

    Renamed to rm_earlier_than.sh

--- rm_earlier_than.sh
+++ rm_earlier_than.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh
+
+#
+# This script is meant to be ran as a cron job every end of the month
+#
+
 BEFORE=`date -I`
 THEPATH="."
 #TODO
 # if month is 1 make it to 12
 # commit for now
+echo $BEFORE
 /usr/bin/find $THEPATH -type f -not -newermt $BEFORE
